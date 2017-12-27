bdaa5344c11a9ac9d28478721e96e09d77d6bbb3
qworld <mail2q@gmail.com>
2017-12-27 16:36:00 +0800

    change git shell

--- shell/git_integration.sh
+++ shell/git_integration.sh
@@ -1,9 +1,19 @@
 #! /bin/bash
 
+sys_log="/var/log/node_git/node_git.log"
+
+function log_info(){
+  local date=`date`
+  local para=$1
+  echo "log info:$date $1" >> $sys_log
+}
+
 cd /var/www/weconnect/
 git clean -f
 git pull bitbucket master
+log_info "git pull done"
 chown www-data:www-data -R /var/www/weconnect/
+log_info "change dir owner done"
 yarn install
 #yarn run test
 #yarn run prd
