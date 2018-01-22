800ddf17ce30beb2d13794aa30388b2c9bf951d7
qworld <mail2q@gmail.com>
2018-01-22 18:13:59 +0800

    modify feature about get token

--- models/token.js
+++ models/token.js
@@ -4,14 +4,15 @@ const monk = require('../lib/data_monk');
 
 const collection = monk.get('access_token');
 //token is json format. {appid, access_token, request_time, expires_in_sec, expired_time }
-exports.token_add = function(token){
+exports.addToken = function(token){
     if(!token){
         console.error("empty token");
     }
-    collection.insert( [{appid:token.appid, 
+    collection.insert( {
+        appid:token.appid, 
         token: token.access_token, 
         request_time: token.request_time, 
         expires_in_sec: token.expires_in_sec,
         expires_in_sec: token.expired_time
-    }] )
+    } )
 }
--- routes/tokens.js
+++ routes/tokens.js
@@ -1,13 +1,48 @@
 const router = require('koa-router')();
+const https = require('https');
+//official url for get token: https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=APPID&secret=APPSECRET";
+const baseUrl = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential";
+
 
 router.prefix('/tokens');
 
 router.get('/', function (ctx, next) {
-  ctx.body = 'this is a token response!';
+    ctx.body = 'this is a token response!';
 });
 
 router.get('/get', function (ctx, next) {
-  ctx.body = 'this is a users/bar response';
+    let domain = 'api.weixin.qq.com';
+    let path = '/cgi-bin/token?grant_type=client_credential';
+    let requestPath = path + "&appid=" + "wx90bbba88d31e8381";
+    requestPath += "&secret=" + "63b3a9188d3dfb3acf5c63c28ec1b05ß";
+
+    const options = {
+        hostname: domain,
+        port: 443,
+        path: path,
+        method: 'GET'
+      };
+      
+    const req = https.request(options, (res) => {
+    console.log('状态码：', res.statusCode);
+    console.log('请求头：', res.headers);
+    
+    res.on('data', (d) => {
+        process.stdout.write(d);
+    });
+    });
+      
+    req.on('error', (e) => {
+    console.error(`请求遇到问题: ${e.message}`);
+    });
+      
+    // 写入数据到请求主体
+    //req.write(postData);
+    req.end();
+      
+      
 });
+
+
 
 module.exports = router;
