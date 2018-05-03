0cf25976a345069cb71a3907789c38ef4e97204f
qworld <mail2q@gmail.com>
2018-05-03 17:53:00 +0800

    change structure

--- models/token.js
+++ models/token.js
@@ -9,6 +9,19 @@ const tokenUri = "https://api.weixin.qq.com/cgi-bin/token?";
 const prj_appid = 'wx90bbba88d31e8381';
 const prj_secret = '63b3a9188d3dfb3acf5c63c28ec1b050';
 
+//define Schema for access token
+const accessTokenSchema = mongoose.Schema({
+    appid: {type:String, required:true, min:5},
+    token: {type:String, required:true, min:10},
+    expires_in_ms: {type:Number, required:true},
+    expires_time_ms: {type:Number, required:true},
+    request_time: {type:String, required:true},
+    expires_time: {type:String, required:true}
+});
+
+//create model from Schema
+var AccessToken = mongoose.model('access_token', accessTokenSchema);
+
 //take charge of access token's actions
 var accessTokensMgr = {
     //form url for get token 
@@ -32,53 +45,66 @@ var accessTokensMgr = {
 
     // private method, get token from db when token isn't expired
     async _getTokenFromCache(){
-        let token = collection.findOne({appid:'wx90bbba88d31e8381'}).then( function(doc){
-            return resolve(doc);
-        } );
+        let token = null;
+        AccessToken.find( {appid: serviceConfigs.service_01.appid} ).lean().exec(function(err, docs){
+            if(err){
+                console.log("error: failed in finding docs");
+            }
+            if(docs.length > 0){
+                console.log("get token from db successfully");
+                token = docs;
+            }
+        });
         return token;
     },
 
     // private method, get token from wechat API
     async _getTokenFromAPI(){
-        let requestTime = Date.now();
-        //wechat's api will return data like {"access_token":"ACCESS_TOKEN","expires_in":7200}
-        var accessToken = await r2.get(this.requestUrl).json;
-        console.log(accessToken);
-        //console.log(this.requestUrl);
-        //减掉60秒，用以避免各类原因造成的误差, reduce 60s for error range
-        let expireTime = requestTime + (accessToken.expires_in - 60) * 1000;
-        accessToken.appid = prj_appid;
-        accessToken.request_time = requestTime;
-        accessToken.expire_time = expireTime;
-        return accessToken;
+    //wechat's api will return data in JSON like {"access_token":"ACCESS_TOKEN","expires_in":7200}
+    let accessToken = await utils.request.getJSON(this.requestUrl);
+
+    if(!accessToken){
+        console.log("error: failed in getting token from wechat's api");
+        return null;
+    }
+
+    //减掉60秒，用以避免各类原因造成的误差, reduce 60s for error range
+    let requestTime = Date.now();
+    let expiresTime = requestTime + (accessToken.expires_in - 60) * 1000;
+    //add new data to JSON
+    accessToken.appid = serviceConfigs.service_01.appid;
+    //accessToken.expires_in_ms = expires_in;
+    accessToken.expires_time_ms = expiresTime;
+    accessToken.request_time = (new Date(requestTime)).toLocaleString("zh-cn", {hour12: false});
+    accessToken.expires_time = (new Date(expiresTime)).toLocaleString("zh-cn", {hour12: false});
+    return accessToken;
     },
 
     // private method, save token into db
     async _saveToken(token){
         if(!token){
             console.error("empty token");
+            return false;
         }
-        //ensure only one record stored in collection
-        //update the record everytime if needed
-        const recordsNum = collection.count();
-        if(recordsNum == 1) {
-            return collection.findOneAndUpdate({appid:appid}, {
-                token: token.access_token, 
-                request_time: token.request_time,
-                expires_in: token.expires_in,
-                expire_time: token.expire_time
-            });
-        }
-        //if records more than 1 or zero, remove all records, 
-        //then insert 1 new record
-        collection.remove(); 
-        collection.insert( {
+        console.log("token is: " + JSON.stringify(token));
+    
+        let newToken = new AccessToken({
             appid:token.appid, 
             token: token.access_token, 
-            request_time: token.request_time, 
-            expires_in: token.expires_in,
-            expire_time: token.expire_time
-        } );
+            expires_in_ms: token.expires_in, 
+            expires_time_ms: token.expires_time_ms,
+            request_time: token.request_time,
+            expires_time: token.expires_time
+        });
+    
+        newToken.save(function(err, doc){
+            if(err){
+                console.log(err);
+                return false;
+            }
+            console.log("saving token into db " + doc);
+            return true;
+        });    
     }
 }
 
--- routes/tokens.js
+++ routes/tokens.js
@@ -1,18 +1,18 @@
 const router = require('koa-router')();
-const weconnect_api = require('/Users/quan/Dev/nodejs/weconnect_api');
+const weconnect_api = require('/Users/quan/Dev/nodejs/weconnect_core');
 //const weApi = require('co-wechat-api');
 //const mongoose = require('mongoose');
 
 router.prefix('/tokens');
 
-router.get('/', function (ctx, next) {
-    ctx.body = 'this is a token response!';
+router.get('/', async function (ctx, next) {
+    let msg = await weconnect_api.utils.getErrMsgByCode(40016);
+    ctx.body = 'this is a token response: ' + msg;
 });
 
 router.get('/get', async function (ctx, next) {
     //console.log('token');
     let token = await weconnect_api.accessToken.getAccessToken();
-    
 
     await ctx.render('common_response', {
         title: 'get token!',
