8e809550c674611f5eaa95f11c849f15ff4b382e
qworld <mail2q@gmail.com>
2018-07-20 17:48:10 +0800

    change verify function

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
+const log4js = require('log4js');
+log4js.configure('./config/config_log4js.json');
+const infologger = log4js.getLogger("default");
+const errlogger = log4js.getLogger("error");
+
 const Koa = require('koa');
 const app = new Koa();
 const views = require('koa-views');
 const json = require('koa-json');
 const onerror = require('koa-onerror');
 const bodyparser = require('koa-bodyparser');
-const logger = require('koa-logger');
+const koa_logger = require('koa-logger');
 const index = require('./routes/index');
 const tokens = require('./routes/tokens');
 const users = require('./routes/users');
@@ -20,7 +25,7 @@ global.ApiPrefix = "https://api.weixin.qq.com/cgi-bin/";
 onerror(app);
 
 // middlewares
-app.use(logger());
+app.use(koa_logger());
 app.use(bodyparser({
   enableTypes:['json', 'form', 'text']
 }));
@@ -39,7 +44,9 @@ app.use(async (ctx, next) => {
   const ms = new Date() - start;
   // x-response-time
   ctx.set('X-Response-Time', `${ms}ms`);
-  console.log(`${ctx.method} ${ctx.url} - ${ms}ms`);
+  let msg = `${ctx.method} ${ctx.url} - ${ms}ms`;
+  console.log(msg);
+  infologger.info(msg);
 });
 
 // routes
@@ -52,11 +59,13 @@ app.use(verify.routes(), verify.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
+  errlogger.error('server error', err, ctx);
 });
 
 app.on('unhandledRejection', (reason, p) => {
   console.log('Unhandled Rejection at:', p, 'reason:', reason);
+  errlogger.fatal('Unhandled Rejection at:', p, 'reason:', reason);
   // application specific logging, throwing an error, or other logic here
 });
 
