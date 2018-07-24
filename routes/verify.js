ec0f34ccfba5a1434dc57dc517050bf6bcff5f82
qworld <mail2q@gmail.com>
2018-07-24 10:12:30 +0800

    npm updates

--- routes/verify.js
+++ routes/verify.js
@@ -1,6 +1,9 @@
 /**
  * 用于第一次开通公众平台接口验证
  * 在公众平台后台提交验证的URL为：xxx.xxx.xxx/verify/，具体域名根据实际情况填写
+ * 官方验证说明：开发者通过检验signature对请求进行校验。若确认此次GET请求来自微信服务器，请原样返回echostr参数内容，则接入生效，成为开发者成功，否则接入失败。
+ * 加密/校验流程如下：将token、timestamp、nonce三个参数进行字典序排序； 2）将三个参数字符串拼接成一个字符串进行sha1加密； 3）开发者获得加密后的字符串可与signature对比，标识该请求来源于微信
+ * 文档地址：https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1421135319
  **/
 
 'use strict';
