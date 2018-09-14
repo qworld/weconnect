d24b4c103d2cc2ceebf53f2412449af4f62e0fc3
qworld <mail2q@gmail.com>
2018-09-14 18:30:16 +0800

    add mongoose for menu

--- models/menu.js
+++ models/menu.js
@@ -7,12 +7,18 @@ const mongoose = require('../lib/mongoDB');
 
 const config_path = global.AppRoot + "/config/menu_list.json";
 
-const schema = mongoose.Schema;
-const model_menu = new schema({
-    appid: { type:String, required:true }
+//const schema = mongoose.Schema;
+const menu_schema = new mongoose.Schema({
+    appid: { type:String, required:true },
+    menu: { type: String, required:true },
+    last_update: { type: Date, default: Date.now }
 });
+
+const menu_model = mongoose.model('Menu', menu_schema);
 
 module.exports.get = async () => {
     let menu_config = await util.promisify(fs.readFile)(config_path, 'utf8');
     return menu_config;
 }
+
+let m = new menu_model()
